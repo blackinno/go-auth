e6aa229fae3237e3576f897094fbc7a93e3ae814
blackinno <ruthamnong.thotsaphon@gmail.com>
2022-08-22 22:59:05 +0700

    update oauth2 for google register or login

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"auth.jwt.api/database"
 	"auth.jwt.api/models"
@@ -9,6 +12,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 var googleConf *oauth2.Config
@@ -17,6 +21,19 @@ type registerRequest struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 	Fullname string `json:"fullname" binding:"required"`
+}
+
+func init() {
+	googleConf = &oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_SECRET"),
+		RedirectURL:  "http://localhost:8080/google/callback",
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+		},
+		Endpoint: google.Endpoint,
+	}
 }
 
 func Register(c *gin.Context) {
@@ -96,6 +113,53 @@ func Login(c *gin.Context) {
 }
 
 func GoogleAuth(c *gin.Context) {
+	redirectUrl := googleConf.AuthCodeURL("state")
+	c.Redirect(http.StatusTemporaryRedirect, redirectUrl)
 }
 
-func GoogleAuthCallback(c *gin.Context) {}
+type googleUser struct {
+	Sub           string `json:"sub"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Picture       string `json:"picture"`
+	Email         string `json:"email"`
+	EmailVerified string `json:"email_verified"`
+	Locale        string `json:"locale"`
+}
+
+func GoogleAuthCallback(c *gin.Context) {
+	state := c.Query("state")
+
+	if state != "state" {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+	code := c.Query("code")
+
+	token, err := googleConf.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+	client := googleConf.Client(oauth2.NoContext, token)
+
+	res, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	defer res.Body.Close()
+
+	content, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	var user googleUser
+	json.Unmarshal(content, &user)
+
+	c.JSON(http.StatusOK, user)
+}
